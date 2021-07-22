f83dcc51f23b525c26ac0698dced79d5b985296c
Chibuike Ukandu <jaydencolins@gmail.com>
2021-07-22 13:35:20 +0100

    added

--- PORTFOLIO/js/main.js
+++ PORTFOLIO/js/main.js
@@ -1,5 +1,10 @@
+const hamburger = document.querySelector('#hamburger');
 const inputField = document.querySelectorAll('input');
 const submitButton = document.getElementById('send');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+});
 
 for(const input of inputField){
     input.addEventListener('click', () => {
