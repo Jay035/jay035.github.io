68803dea80512ca797ee073bc7f2791014b1d6e7
Chibuike Ukandu <jaydencolins@gmail.com>
2021-12-31 23:33:10 +0100

    update

--- launch-countdown-timer-main/app.js
+++ launch-countdown-timer-main/app.js
@@ -6,7 +6,7 @@ window.addEventListener("load", function(){
 
 const countdown = () => {
     // launch time 
-    const endDate = new Date("Jan 01, 2022 00:00:00").getTime();
+    const endDate = new Date("Mar 31, 2022 00:00:00").getTime();
     // present date 
     const now = new Date().getTime();
     const diff = endDate - now;
@@ -22,13 +22,44 @@ const countdown = () => {
     let timeSeconds = Math.floor((diff % minutes) / seconds);
 
     timeHours = timeHours < 10 ? '0' + timeHours : timeHours;
+    timeHours = timeHours < 00 ? timeHours : timeHours;
     timeMinutes = timeMinutes < 10 ? '0' + timeMinutes : timeMinutes;
+    timeMinutes = timeMinutes < 00 ? timeMinutes : timeMinutes;
     timeSeconds = timeSeconds < 10 ? '0' + timeSeconds : timeSeconds;
+    timeSeconds = timeSeconds < 00 ? timeSeconds : timeSeconds;
 
-    document.getElementById('days').innerText = timeDays;
-    document.getElementById('hours').innerText = timeHours;
-    document.getElementById('minutes').innerText = timeMinutes;
-    document.getElementById('seconds').innerText = timeSeconds;
+    const daysFlip = document.querySelectorAll('[data-group="days"]');
+    const hoursFlip = document.querySelectorAll('[data-group="hours"]');
+    const minutesFlip = document.querySelectorAll('[data-group="minutes"]');
+    const secondsFlip = document.querySelectorAll('[data-group="seconds"]');
+
+    daysFlip.forEach(elem => {
+        timeHours == 00
+            ? (elem.parentElement.parentElement.classList.add('play'), elem.parentElement.parentElement.classList.remove('play'))
+            : elem.parentElement.parentElement.classList.remove('play');
+
+        elem.innerHTML = timeDays;
+    });
+
+    hoursFlip.forEach(elem => {
+        timeMinutes == 00
+            ? elem.parentElement.parentElement.classList.add('play')
+            : elem.parentElement.parentElement.classList.remove('play');
+
+        elem.innerHTML = timeHours;
+    });
+
+    minutesFlip.forEach(elem => {
+        timeSeconds == 00
+            ? elem.parentElement.parentElement.classList.add('play')
+            : elem.parentElement.parentElement.classList.remove('play');
+        
+        elem.innerHTML = timeMinutes;
+    }); 
+
+    secondsFlip.forEach(elem => {
+        elem.innerHTML = timeSeconds;
+    });
 }
 
 setInterval(countdown, 1000);
